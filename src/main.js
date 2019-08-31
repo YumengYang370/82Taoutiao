e8808ee9154a03a528e660a8ab71b9f61f5a3249
杨雨蒙 <yumengyang@163.com>
2019-08-31 21:32:14 +0800

    安装elementUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'  // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化样式 包括 margin padding li
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)// 全局注册elementUI组件 任意位置都可以使用
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
