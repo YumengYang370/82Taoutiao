4cd89ca04171cb2872abb10f9fad07336cd03db5
杨雨蒙 <yumengyang@163.com>
2019-08-31 21:25:59 +0800

    这是测试

---
+++ src/child-b.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{name}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: "HelloWorld"
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
