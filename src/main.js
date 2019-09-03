45b3d22895f030803017028c34a89b208197652b
杨雨蒙 <yumengyang@163.com>
2019-09-03 21:27:41 +0800

    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'  // 引入UI
+import ElementUI from 'element-ui'
+
+// import Component from './components/index'//相当于下边 index可忽略不写
+import Component from './components' // 自己注册的全局自定义组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './style/index.less' // 引入初始化样式 包括 margin padding li
+import axios from 'axios'// 引入axios插件
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值基础地址
+Vue.prototype.$axios = axios // 给Vue对象的原型属性(Vue中属性都以$开头)赋值axios 那么所有vue实例自动拥有$axios
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)// 全局注册elementUI组件 任意位置都可以使用
-new Vue({
+
+Vue.use(ElementUI) // 实例化Vue前引入 全局注册elementUI组件 任意位置都可以使用
+Vue.use(Component) // 实例化Vue前注册引用 自己写的全局注册自定义组件
+
+new Vue({ // 实例化Vue
   router,
   render: h => h(App)
 }).$mount('#app')
