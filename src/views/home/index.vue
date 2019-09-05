d0fce5f58aa09d6be0d2c99df006d458fcefde76
杨雨蒙 <yumengyang@163.com>
2019-09-05 18:30:43 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div >
-      home
+  <div>
+    <!-- 最外层容器 -->
+    <el-container>
+      <!-- 左侧边栏 -->
+      <el-aside>左侧</el-aside>
+
+      <!-- 右侧 -->
+      <el-container>
+        <!-- 头部 -->
+        <el-header>头部</el-header>
+        <!-- 主体 -->
+        <el-main>主体</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
